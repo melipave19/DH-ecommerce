a3e91ac133886b88f6fae0cf357ddc9c6436f4c9
melipave_19@gmail.com <melipave_19@gmail.com>
2019-12-27 20:05:02 -0300

    validacion en modal

--- form.php
+++ form.php
@@ -1,3 +1,45 @@
+<?php
+
+
+// Validar campo Email:
+if(isset ($_POST [ 'email'])) {
+if (empty ($_POST  [ 'email'])) {
+$errores [ 'email'] [ ] = "Este campo es obligatorio";
+}
+if(!filter_var($_POST [ 'email'], FILTER_VALIDATE_EMAIL)) {
+$errores ['email'][]= "Debes ingresar un email valido";
+}
+}
+//Validar campo Last Name
+if(isset ($_POST [ 'name'])) {
+if (empty ($_POST  [ 'name'])) {
+$errores ['name'] [ ] = "Este campo es obligatorio";
+}
+if($_POST['password'] <6 ) {
+$errores ['name'][] = "Este campo debe tener como minimo 6 caracteres";
+}
+}
+
+//Validar campo password
+if(isset ($_POST [ 'password'])) {
+if (empty ($_POST  [ 'password'])) {
+$errores [ 'password'] [ ] = "Este campo es obligatorio";
+}
+if(strlen($_POST['password']) < 6 ) {
+$errores ['password'][] = "Tu contraseña debe tener al menos 6 caracteres";
+}
+}
+
+//Validar campo repassword
+if(isset ($_POST [ 'repassword'])) {
+if (empty ($_POST  [ 'repassword'])) {
+$errores [ 'repassword'] [ ] = "Este campo es obligatorio";
+}
+if($_POST['password'] != $_POST['repassword']) {
+$errores ['repassword'][] = "las contraseñas deben coincidir";
+}
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -20,37 +62,37 @@
     }
 
     .form-signin-heading,
-    	.checkbox {
-    	  margin-bottom: 30px;
-    	}
+        .checkbox {
+          margin-bottom: 30px;
+        }
 
-    	.checkbox {
-    	  font-weight: normal;
-    	}
+        .checkbox {
+          font-weight: normal;
+        }
 
-    	.form-control {
-    	  position: relative;
-    	  font-size: 16px;
-    	  height: auto;
-    	  padding: 10px;
-    		@include box-sizing(border-box);
+        .form-control {
+          position: relative;
+          font-size: 16px;
+          height: auto;
+          padding: 10px;
+            @include box-sizing(border-box);
       }
 
-    		&:focus {
-    		  z-index: 2;
-    		}
+            &:focus {
+              z-index: 2;
+            }
 
-    	input[type="text"] {
-    	  margin-bottom: 20px;
-    	  border-bottom-left-radius: 0;
-    	  border-bottom-right-radius: 0;
-    	}
+        input[type="text"] {
+          margin-bottom: 20px;
+          border-bottom-left-radius: 0;
+          border-bottom-right-radius: 0;
+        }
 
-    	input[type="password"] {
-    	  margin-bottom: 20px;
-    	  border-top-left-radius: 0;
-    	  border-top-right-radius: 0;
-    	}
+        input[type="password"] {
+          margin-bottom: 20px;
+          border-top-left-radius: 0;
+          border-top-right-radius: 0;
+        }
 
   </style>
 </head>
@@ -132,36 +174,36 @@
           </div>
           <div class="modal-body">
             <div class="wrapper">
-            <form class="form-signin" method="post">
-                <input type="text" class="form-control" name="username" placeholder="Email Address" required="" autofocus="" />
+            <form class="form-signin" method="POST">
+                <input type="text" class="form-control" name="email" placeholder="Email Address" autofocus="" value="<?php echo $_POST['email'] ?>" />
                 <?php
-                if(isset($errores ['Email'])) {
-                foreach($errores [ 'Email' ] as $error) {
-                echo "<small class='text-danger'>" . $error . '</small>';
+                if(isset($errores ['email'])) {
+                foreach($errores [ 'email' ] as $error) {
+                echo "<small class='text-danger'>" . $error . "</small>";
                 }
                 }
                  ?>
-                <input type="text" class="form-control" name="name" placeholder="Last Name" required="" autofocus="" />
+                <input type="text" class="form-control" name="name" placeholder="Last Name"  autofocus="" />
                 <?php
-                if(isset($errores ['nombre'])) {
-                foreach($errores [ 'nombre' ] as $error) {
-                echo "<small class='text-danger'>" . $error . '</small>';
+                if(isset($errores ['name'])) {
+                foreach($errores [ 'name' ] as $error) {
+                echo "<small class='text-danger'>" . $error . "</small>";
                 }
                 }
                  ?>
-                <input type="password" class="form-control" name="password" placeholder="Password" required=""/>
+                <input type="password" class="form-control" name="password" placeholder="Password" />
                 <?php
                 if(isset($errores ['password'])) {
                 foreach($errores [ 'password' ] as $error) {
-                echo "<small class='text-danger'>" . $error . '</small>';
+                echo "<small class='text-danger'>" . $error . "</small>";
                 }
                 }
                  ?>
-                <input type="password" class="form-control" name="repassword" placeholder="Confirm Password" required=""/>
+                <input type="password" class="form-control" name="repassword" placeholder="Confirm Password"/>
                 <?php
-                if(isset($errores ['repassword'])) {
+                if(isset($errores['repassword'])) {
                 foreach($errores [ 'repassword' ] as $error) {
-                echo "<small class='text-danger'>" . $error . '</small>';
+                echo "<small class='text-danger'>" . $error . "</small>";
                 }
                 }
                  ?>
@@ -174,7 +216,7 @@
             <?php
             if(isset($errores ['nombre'])) {
             foreach($errores [ 'nombre' ] as $error) {
-            echo "<small class='text-danger'>" . $error . '</small>';
+            echo "<small class='text-danger'>" . $error . "</small>";
             }
             }
              ?>
@@ -186,46 +228,10 @@
       <!-- Modal content-->
 </div>
 <?php
-
-
-// Validar campo Email:
-if(isset ($_POST [ 'email'])) {
-if (empty ($_POST  [ 'email'])) {
-$errores [ 'email'] [ ] = "Este campo es obligatorio";
-}
-if(!filter_var($_POST [ 'email'], FILTER_VALIDATE_EMAIL)) {
-$errores ['email'][]= "Debes ingresar un email valido";
-}
-}
-//Validar campo Last Name
-if(isset ($_POST [ 'name'])) {
-if (empty ($_POST  [ 'name'])) {
-$errores ['name'] [ ] = "Este campo es obligatorio";
-}
-if($_post['password'] <6 ) {
-$errores ['name'][] = "Este campo debe tener como minimo 6 caracteres";
-}
-}
-
-//Validar campo password
-if(isset ($_POST [ 'password'])) {
-if (empty ($_POST  [ 'password'])) {
-$errores [ 'password'] [ ] = "Este campo es obligatorio";
-}
-if(strlen($_POST['password']) < 6 ) {
-$errores ['password'][] = "Tu contraseña debe tener al menos 6 caracteres";
-}
-}
-
-//Validar campo repassword
-if(isset ($_POST [ 'repassword'])) {
-if (empty ($_POST  [ 'repassword'])) {
-$errores [ 'repassword'] [ ] = "Este campo es obligatorio";
-}
-if($_post['password'] != $_POST['repassword']) {
-$errores ['repassword'][] = "las contraseñas deben coincidir";
-}
-}
-if (count($errores) == 0 ) {
-// El usuario puso todo bien
+if(isset($errores)) {
+  ?>
+    <script>
+      $('#signIn').modal();
+    </script>
+  <?php
 }
